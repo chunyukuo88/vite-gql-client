8712473186434aa2baa3d448391dc65c8ce33fd7
chunyukuo88 <alexgochenour@gmail.com>
2024-01-22 20:32:41 -0500

    Added a 'down for maintenance' message.

--- src/App.tsx
+++ src/App.tsx
@@ -18,45 +18,53 @@ function App() {
         fontWeight: 'bold',
     };
 
-    const RecentGigs = () => (
+    const ServerDownForMaintenance = () => (
         <section id='gigs-container'>
-            {gigs && gigs.map((gig) => {
-                return (
-                    <div className='gig-card card' key={gig.task_id}>
-                        <div className='card-header'>Gig completed {gig.end_date.split(' ')[0]}</div>
-                        <h3>
-                            {gig.date_paid
-                                ? <span className='payment-complete'>Paid on {gig.date_paid.split('T')[0]}</span>
-                                : <span style={{ color: 'salmon'}}>Payment pending.</span>
-                            }
-                        </h3>
-                        <div>
-                            <span className='card-field'>Earnings: </span>
-                            <span style={dollarStyle} >${gig.total_amount_charged}</span>
-                        </div>
-                        <div>
-                            <span className='card-field'>Documents: </span>
-                            {gig.file_names}
-                        </div>
-                        <div>
-                            <span className='card-field'>Company: </span>
-                            {gig.company.dba}
-                        </div>
-                        <div>
-                            <span className='card-field'>Contact: </span>
-                            {gig.company.contact_name}
-                        </div>
-                    </div>
-                )
-            })}
+            The server is undergoing scheduled maintenance. Please try again tomorrow.
         </section>
+    );
+
+    const RecentGigs = () => (
+        <>
+            <div className='is-size-2'>Gigs</div>
+            <div className='is-size-4'>Recently completed gigs</div>
+            <section id='gigs-container'>
+                {gigs && gigs.map((gig) => {
+                    return (
+                        <div className='gig-card card' key={gig.task_id}>
+                            <div className='card-header'>Gig completed {gig.end_date.split(' ')[0]}</div>
+                            <h3>
+                                {gig.date_paid
+                                    ? <span className='payment-complete'>Paid on {gig.date_paid.split('T')[0]}</span>
+                                    : <span style={{ color: 'salmon'}}>Payment pending.</span>
+                                }
+                            </h3>
+                            <div>
+                                <span className='card-field'>Earnings: </span>
+                                <span style={dollarStyle} >${gig.total_amount_charged}</span>
+                            </div>
+                            <div>
+                                <span className='card-field'>Documents: </span>
+                                {gig.file_names}
+                            </div>
+                            <div>
+                                <span className='card-field'>Company: </span>
+                                {gig.company.dba}
+                            </div>
+                            <div>
+                                <span className='card-field'>Contact: </span>
+                                {gig.company.contact_name}
+                            </div>
+                        </div>
+                    )
+                })}
+            </section>
+        </>
     );
 
     return (
         <main id='App'>
-            <div className='is-size-2'>Gigs</div>
-            <div className='is-size-4'>Recently completed gigs</div>
-            <RecentGigs />
+            {gigs ? <RecentGigs/> : <ServerDownForMaintenance />}
         </main>
     )
 }
