a8c776af73a6cf3eea342f36f29ed8f060d14a93
chunyukuo88 <alexgochenour@gmail.com>
2024-02-04 14:39:50 -0500

    Sign in API call is successful. Next: Apply to global state and possibly session storage.

--- README.md
+++ README.md
@@ -13,4 +13,4 @@
 ### Good references:
 
 - A [refresher](https://www.sammeechward.com/cognito-user-pool-react) on integrating Cognito into your React app by Sam Meacham-Ward.
-- [Amplify API docs](https://docs.amplify.aws/javascript/build-a-backend/auth/enable-sign-up/)
+- This app was not created using the Amplify framework, I am just using the auth API. It took a while to find the docs for doing it this way, so [here](https://docs.amplify.aws/react/tools/libraries/configure-categories/#pageMain) it is.
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router';
 import { createBrowserRouter } from 'react-router-dom';
+import {Amplify} from 'aws-amplify';
 
 import Root from './Root.tsx';
 import Gigs from './pages/Gigs.tsx';
 import LogIn from './pages/LogIn.tsx';
+
+Amplify.configure({
+    Auth: {
+        Cognito: {
+            userPoolClientId: '54f5a0ak2p6q99uje0s5arfgj2',
+            userPoolId: 'us-east-2_NpmEssPrY',
+            loginWith: { username: 'true' },
+        },
+    },
+});
 
 const router = createBrowserRouter([
     {path: '/', element: <Gigs />},
